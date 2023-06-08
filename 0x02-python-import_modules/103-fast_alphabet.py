3fd79c289ecc304b354542b5e5faf785705e82c2
Nomad-cyber <morrismaingi2@gmail.com>
2023-06-08 20:24:12 +0000

    0x02. Python - import & modules

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import string
+"""Program prints the alphabet in uppercase, followed by a new line."""
+print(string.ascii_uppercase)
---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Program imports and prints the variable a."""
+    from variable_load_5 import a
+
+    print(a)

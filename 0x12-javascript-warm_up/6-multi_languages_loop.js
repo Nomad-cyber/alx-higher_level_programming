1964fc872822231f07d7fd67401477af7a0c340b
Nomad-cyber <morrismaingi2@gmail.com>
2023-09-13 18:09:12 +0000

    0x12. JavaScript - Warm up

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// loop through 3 messages to print to console
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < 3; i++) {
+  console.log(arr[i]);
+}
